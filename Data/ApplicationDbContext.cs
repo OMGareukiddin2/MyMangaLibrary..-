ef647b6a938a15b6c30ee847556ccd3ed3986568
randomZname <58574142+randomZname@users.noreply.github.com>
2022-08-22 00:56:59 +0300

    Add files via upload
    database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyMangaLibrary.Models;
+
+namespace MyMangaLibrary.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyMangaLibrary.Models.Manga> Manga { get; set; }
+        public DbSet<MyMangaLibrary.Models.Mangaka> Mangaka { get; set; }
+        public DbSet<MyMangaLibrary.Models.Characters> Characters { get; set; }
+    }
+}
